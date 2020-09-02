70842b2eaf11a1a86a1a0d5f27bf9d3bb86b3bd6
Life4honor <choejy0329@gmail.com>
2020-09-02 23:56:04 +0900

    Day 1

--- blog/posts/src/index.js
+++ blog/posts/src/index.js
@@ -1,13 +1,26 @@
 import express from "express";
+import cors from 'cors';
+import { randomBytes } from "crypto";
 
 const app = express();
+app.use(express.json())
+app.use(cors())
+
+const posts = {};
 
 app.get('/posts', (req, res) => {
-
+    res.send(posts)
 });
 
-app.post('posts', (req, res) => {
+app.post('/posts', (req, res) => {
+    const id = randomBytes(4).toString('hex');
+    const { title } = req.body;
 
+    posts[id] = {
+        id, title
+    };
+
+    res.status(201).send(posts[id]);
 });
 
 app.listen(4000, () => {

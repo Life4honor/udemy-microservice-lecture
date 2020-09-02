ff8e39239db8ff26ae2e828da8506f194f9c2cc6
Life4honor <choejy0329@gmail.com>
2020-09-03 00:18:23 +0900

    Day 2
    Patch 0

--- blog/client/src/App.js
+++ blog/client/src/App.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import PostCreate from './PostCreate';
+import PostList from './PostList';
 
 export default () => {
     return <div className='container'>
         <h1>CreatePost</h1>
         <PostCreate />
+        <h1>Posts</h1>
+        <PostList />
     </div>;
 };

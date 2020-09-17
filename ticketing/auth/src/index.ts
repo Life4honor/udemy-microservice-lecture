ee4445b1f7bae850c50b8ccf4190d6d473a81684
Life4honor <choejy0329@gmail.com>
2020-09-18 00:01:35 +0900

    Day 8
    Patch 1

--- ticketing/auth/src/index.ts
+++ ticketing/auth/src/index.ts
@@ -1,18 +1,25 @@
 import cors from "cors";
 import express from "express";
+import "express-async-errors";
+import { NotFoundError } from "./errors/not-found-error";
 import { errorHandler } from "./middlewares/error-handlers";
 import { currentUserRouter } from "./routes/current-user";
 import { signinRouter } from "./routes/signin";
 import { signoutRouter } from "./routes/signout";
 import { signupRouter } from "./routes/signup";
 const app = express();
-app.use(express.json());
 app.use(cors());
+app.use(express.json());
 
 app.use(currentUserRouter);
 app.use(signinRouter);
 app.use(signoutRouter);
 app.use(signupRouter);
+
+app.all("*", async (req, res) => {
+  throw new NotFoundError();
+});
+
 app.use(errorHandler);
 
 app.listen(3000, () => {

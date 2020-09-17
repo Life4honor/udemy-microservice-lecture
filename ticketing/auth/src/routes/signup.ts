f721cf4a8572691d0f8de4d76eaedcae93e67e92
Life4honor <choejy0329@gmail.com>
2020-09-17 22:51:01 +0900

    Day 7
    Patch 2

--- ticketing/auth/src/routes/signup.ts
+++ ticketing/auth/src/routes/signup.ts
@@ -1,6 +1,9 @@
 import { body, validationResult } from 'express-validator';
 import express, { Request, Response } from 'express';
 
+import { DatabaseConnectionError } from '../errors/database-connection-error';
+import { RequestValidationError } from '../errors/request-validation-error';
+
 const router = express.Router();
 
 router.post(
@@ -18,15 +21,13 @@ router.post(
         const errors = validationResult(req);
 
         if (!errors.isEmpty()) {
-            const error = new Error('Invalid email or password');
-            error.reasons = errors.array();
-            throw error
+            throw new RequestValidationError(errors.array());
         }
 
         const { email, password } = req.body;
 
         console.log('Creating a uesr...');
-        throw new Error('Error connecting to database');
+        throw new DatabaseConnectionError();
 
         res.send({});
     });

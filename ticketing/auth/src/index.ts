5ef77897677fcd1968aa2b7fe4fd78aa1bbeec35
Life4honor <choejy0329@gmail.com>
2020-11-04 22:51:44 +0900

    Day 11
    Patch 1

--- ticketing/auth/src/index.ts
+++ ticketing/auth/src/index.ts
@@ -2,6 +2,7 @@ import cors from "cors";
 import express from "express";
 import "express-async-errors";
 import mongoose from "mongoose";
+import cookieSession from 'cookie-session';
 import { NotFoundError } from "./errors/not-found-error";
 import { errorHandler } from "./middlewares/error-handlers";
 import { currentUserRouter } from "./routes/current-user";
@@ -9,8 +10,15 @@ import { signinRouter } from "./routes/signin";
 import { signoutRouter } from "./routes/signout";
 import { signupRouter } from "./routes/signup";
 const app = express();
+app.set('trust proxy', true);
 app.use(cors());
 app.use(express.json());
+app.use(
+  cookieSession({ 
+    signed: false,
+	secure: true
+  })
+);
 
 app.use(currentUserRouter);
 app.use(signinRouter);

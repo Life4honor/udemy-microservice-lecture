2e7d551204456485d1b0c455e81878f503067240
Life4honor <choejy0329@gmail.com>
2020-11-06 19:46:17 +0900

    Day 13
    Patch 1

--- ticketing/auth/src/routes/signin.ts
+++ ticketing/auth/src/routes/signin.ts
@@ -1,7 +1,10 @@
 import express from "express";
 import { body } from 'express-validator';
+import jwt from 'jsonwebtoken';
 
+import { Password } from '../services/password';
 import { validateRequest } from '../middlewares/validate-request';
+import { BadRequestError } from '../errors/bad-request-error';
 const router = express.Router();
 
 router.post("/api/users/signin", 
@@ -15,7 +18,31 @@ router.post("/api/users/signin",
       .withMessage('You must supply a password')
   ],
   validateRequest,
-  (req: Request, res: Response) => {
+  async (req: Request, res: Response) => {
+    const { email, password } = req.body;
+
+    const existingUser = await User.findOne({ email });
+    if (!existingUser) {
+      throw new BadRequestError('Invalid credentials');
+    }
+
+    const passwordsMatch = await Password.compare(existingUser.password, password);
+    if (!passwordsMatch) {
+      throw new BadRequestError('Invalid credentials');
+    }
+
+	const userJwt = jwt.sign({
+	  id: existingUser.id,
+	  email: existingUser.email
+	}, 
+    process.env.JWT_KEY!
+    );
+
+	req.session = {
+	  jwt: userJwt
+	};
+
+	res.status(200).send(existingUser);
   }
 );
 
